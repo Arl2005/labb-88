70afe563d5b09af12ece9799b83da0a02d1303a6
Arl2005 <157169585+Arl2005@users.noreply.github.com>
2024-03-11 13:08:29 -0400

    Update global-nav.php

--- includes/global-nav.php
+++ includes/global-nav.php
@@ -8,8 +8,6 @@
 					<li><a href="/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
 					<li><a href="/green.php" title="Learn about green blocks">Green Blocks</a></li>
 					<li><a href="/about.php" title="Learn more about us">About</a></li>
-					
-					
 				</ul>
 			</nav>
 <!-- STEP 4: Modify all of the above href relative paths so that they start with '/' - this means that they all point to the host name, and that the files reside in the site's public directory -->
